ac5b1de20b4f90fa09e710ad50661c06e959f868
unknown <colletaxel15@gmail.com>
2017-11-15 11:43:04 +0100

    Initial commit

---
+++ app/src/main/java/com/axelcollet/tp_android_varo_collet/MainActivity.java
@@ -0,0 +1,13 @@
+package com.axelcollet.tp_android_varo_collet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
