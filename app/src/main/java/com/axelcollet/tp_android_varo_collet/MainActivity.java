19389293c122fe571798b983c7509291d6809a12
Axel Collet <colletaxel15@gmail.com>
2017-11-15 12:49:50 +0100

    bcp de #todo pour proposer une lignes de dévellopement

--- app/src/main/java/com/axelcollet/tp_android_varo_collet/MainActivity.java
+++ app/src/main/java/com/axelcollet/tp_android_varo_collet/MainActivity.java
@@ -2,6 +2,9 @@ package com.axelcollet.tp_android_varo_collet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button button_validate = (Button)findViewById(R.id.button);
+        button_validate.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //// TODO: 15.11.2017 vérifier le mot, passer au suivant
+            }
+        });
+
+        final Button button_menu = (Button)findViewById(R.id.button3);
+        button_validate.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //// TODO: 15.11.2017 back  menu
+            }
+        });
+
     }
 }
+
+//// TODO: 15.11.2017 faire classe carton
+//// // TODO: 15.11.2017 faire classe qui contient plusieurs carton genre une liste des ids
+//// TODO: 15.11.2017 faire activity menu
+////// TODO: 15.11.2017 passer la liste de carton choisi dans le menu a cette activité
+// TODO: 15.11.2017 renomer cette activité ?!?
+// TODO: 15.11.2017  stats réussite / echec d'un carton
