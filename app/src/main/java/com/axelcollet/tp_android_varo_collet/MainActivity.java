9ec9dcb9f1ce35bb31fa72e9aadad3b2d5973806
Axel Collet <colletaxel15@gmail.com>
2017-11-23 18:44:46 +0100

    Tentative fragment 1

--- app/src/main/java/com/axelcollet/tp_android_varo_collet/MainActivity.java
+++ app/src/main/java/com/axelcollet/tp_android_varo_collet/MainActivity.java
@@ -2,6 +2,10 @@ package com.axelcollet.tp_android_varo_collet;
 
 import android.app.Activity;
 import android.graphics.Color;
+
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,83 +22,32 @@ import java.util.concurrent.Delayed;
 
 
 // If you want you can add other log definition for info, warning etc
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
-    private TupleVocabulaire tv1 = new TupleVocabulaire("work","travail");
-    private TupleVocabulaire tv2 = new TupleVocabulaire("play","jouer");
-    private TupleVocabulaire tv3 = new TupleVocabulaire("Hello","Bonjour");
-    private TupleVocabulaire tv4 = new TupleVocabulaire("credit card","carte de crédit");
-    private int posList = 0;
-    private TupleVocabulaire tvActuel;
-
-    private List<TupleVocabulaire> tvList = new ArrayList<TupleVocabulaire>(10);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Activity mActivity = this;
+        //fragVoc = (FragmentVoc)getSupportFragmentManager().findFragmentById(R.id.fragment_fragment_voc);
 
-        final Button buttonValidate = (Button) findViewById(R.id.button_validate);
+        // Create an instance of ExampleFragment
+        FragmentVoc fragVoc = new FragmentVoc();
 
-
-
-        tvList.add(tv1);
-        tvList.add(tv2);
-        tvList.add(tv3);
-        tvList.add(tv4);
-        tvActuel = tvList.get(posList);
-
-        initTuble(tvActuel);
-        buttonValidate.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                if (buttonValidate.getText()==getResources().getString(R.string.validate)){
-                    buttonValidate.setText(getResources().getString(R.string.next));
-
-                    verifieTuple(tvActuel);
-                }else{
-                    buttonValidate.setText(getResources().getString(R.string.validate));
-
-                    tvActuel = nextMot();
-                    initTuble(tvActuel);
-                }
+        // In case this activity was started with special instructions from an Intent,
+        // pass the Intent's extras to the fragment as arguments
+        fragVoc.setArguments(getIntent().getExtras());
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_fragment_voc,FragmentVoc.newInstance("lool","lool")).commit();
 
 
-            }
-        });
 
     }
 
-    private void initTuble(TupleVocabulaire tv){
-        final TextView viewMot = (TextView)findViewById(R.id.textView);
-        final EditText viewReponse = (EditText)findViewById(R.id.editText);
-        viewReponse.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
-        viewMot.setText(tv.Mot);
-
+    @Override
+    public void onFragmentInteraction(Uri uri){
+        //you can leave it empty
     }
 
-    private void verifieTuple (TupleVocabulaire tv){
-        final EditText viewReponse = (EditText)findViewById(R.id.editText);
-        final TextView viewMot = (TextView)findViewById(R.id.textView);
-        if (tv.VerifieTuble(viewMot.getText().toString(),viewReponse.getText().toString())){
-            viewReponse.setBackgroundColor(Color.GREEN);
-        }else{
-            viewReponse.setBackgroundColor(Color.RED);
-            viewReponse.setText(tv.MotTraduit);
-
-        }
-    }
-
-    private TupleVocabulaire nextMot(){
-        posList++;
-
-        if (!(posList< tvList.size())){
-            posList =0;
-        }
-        return tvList.get(posList);
-    }
 }
 // TODO: 15.11.2017 faire classe carton
 // TODO: 15.11.2017 faire classe qui contient plusieurs carton genre une liste des ids
