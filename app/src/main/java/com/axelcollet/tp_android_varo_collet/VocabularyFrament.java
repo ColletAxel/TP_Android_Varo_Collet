47975986927b345fdaa093531badd44063e98a31
Axel Collet <colletaxel15@gmail.com>
2017-11-29 14:13:56 +0100

    Fragment + jeu voc fonctionnel #Joie et bonneHumeur
    Le TryHardClub ne meurt JAMAIS !!!!

--- app/src/main/java/com/axelcollet/tp_android_varo_collet/VocabularyFrament.java
+++ app/src/main/java/com/axelcollet/tp_android_varo_collet/VocabularyFrament.java
@@ -1,19 +1,36 @@
 package com.axelcollet.tp_android_varo_collet;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class VocabularyFrament extends Fragment {
+    private TupleVocabulaire tv1 = new TupleVocabulaire("work","travail");
+    private TupleVocabulaire tv2 = new TupleVocabulaire("play","jouer");
+    private TupleVocabulaire tv3 = new TupleVocabulaire("Hello","Bonjour");
+    private TupleVocabulaire tv4 = new TupleVocabulaire("credit card","carte de crédit");
+    private int posList = 0;
+    private TupleVocabulaire tvActuel;
+    private EditText viewReponse;
+    private Button buttonValidate;
 
+    private List<TupleVocabulaire> tvList = new ArrayList<TupleVocabulaire>(10);
 
+    private TextView viewMot;
     public VocabularyFrament() {
         // Required empty public constructor
     }
@@ -22,7 +39,65 @@ public class VocabularyFrament extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.vocabulary_frament_layout, container, false);
+        tvList.add(tv1);
+        tvList.add(tv2);
+        tvList.add(tv3);
+        tvList.add(tv4);
+        tvActuel = tvList.get(posList);
+
+
+
+        View rootView = inflater.inflate(R.layout.vocabulary_frament_layout, container, false);
+        final Button buttonValidate = rootView.findViewById(R.id.button_validate);
+        viewReponse = rootView.findViewById(R.id.editText);
+        viewMot = rootView.findViewById(R.id.textView);
+        initTuble(tvActuel);
+        buttonValidate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (buttonValidate.getText() == getResources().getString(R.string.validate)) {
+                    buttonValidate.setText(getResources().getString(R.string.next));
+
+                    verifieTuple(tvActuel);
+                } else {
+                    buttonValidate.setText(getResources().getString(R.string.validate));
+
+                    tvActuel = nextMot();
+                    initTuble(tvActuel);
+                }
+            }
+        });
+
+        return rootView;
     }
 
+    private void initTuble(TupleVocabulaire tv){
+
+        viewReponse.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        viewMot.setText("\n\n\n" + tv.Mot);
+
+    }
+
+    private void verifieTuple (TupleVocabulaire tv){
+        if (tv.VerifieTuble(viewMot.getText().toString(),viewReponse.getText().toString())){
+            viewReponse.setBackgroundColor(Color.GREEN);
+        }else{
+            viewReponse.setBackgroundColor(Color.RED);
+            viewReponse.setText(tv.MotTraduit);
+
+        }
+    }
+
+    private TupleVocabulaire nextMot(){
+        posList++;
+
+        if (!(posList< tvList.size())){
+            posList =0;
+        }
+        return tvList.get(posList);
+    }
 }
+
+
+
